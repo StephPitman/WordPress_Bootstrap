f61eeb36341eb68c85227f84893c698564e7751e
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-07-09 22:52:42 -0400

    Understanding Section and Field Creation
    -created header section
       -created header colour field

--- bootstrap-plugin.php
+++ bootstrap-plugin.php
@@ -1,26 +1,51 @@
-/**
- * Plugin Name: Bootstrap Implementation (Steph)
- * Plugin URI: http://www.stephanielynnpitman.com/bootstrap-steph
- * Description: Uses Bootstrap as primary visual framework.
- * Version: 1.0
- * Author: Stephanie Pitman
- * Author URI: http://www.stephanielynnpitman.com
- */
+
+/*
+   Plugin Name:Bootstrap Implementation (Steph)
+   Plugin URI: http:www.stephanielynnpitman.com/bootstrap-steph
+   Description: Uses Bootstrap as primary visual framework
+   Version: 1.0
+   Author: Stephanie Pitman
+   Author URI: http://www.stephanielynnpitman.com
+*/
 <?php
-/*creates new page options page, which will be the setting page for the plugin*/
-add_action('admin_menu','add_setting_page');
-function add_setting_page(){
-	add_options_page('Customize Page With Bootstrap', 'Plugin Menu', 'manage_options','plugin','setting_page_creation');
+error_reporting(0);
+add_action('admin_menu','wbs_add_menu');
+add_action('admin_init','wbs_settings_init');
+
+function wbs_add_menu(){
+	add_options_page('WordPress and Bootstrap Settings Page','WordPress and Bootstrap Settings Page','manage_options','wbs-api-page','wbs_page_create');
 }
-/*Creates visual representation of settings page*/
-function setting_page_creation(){
+
+function wbs_page_create(){
 	?>
-	<div>
-		<h2>Customize Page With Bootstrap</h2>
-		<form action="custom_bootstrap.php" method="post">
-			<input name="Submit" type="submit" value="Save Changes"/>
-		</form>
-	</div>
+	<form>
+	<h2>WordPress Bootstrap Settings</h2>
+	<?php
+	settings_fields('wbsHeaderPlugin');
+	do_settings_sections('wbsHeaderPlugin');
+	submit_button();
+	?>
+	</form>
+	<?php
+}
+
+function wbs_settings_init(){
+	register_setting('wbsHeaderPlugin','wbs_header_settings');
+	add_settings_section('wbs_plugin_header_section',__('Header Settings','wordpress'), 'wbs_section_call', 'wbsHeaderPlugin');
+	add_settings_field('wbs_header_colour',__('Colour','wordpress'), 'wbs_select_field_call', 'wbsHeaderPlugin','wbs_plugin_header_section');
+}
+
+function wbs_section_call(){
+	echo __('Customize Your Website Header','wordpress');
+}
+
+function wbs_select_field_call(){
+	$options = get_option('wbs_header_settings');
+	?>
+	<select>
+		<option value = '1'>Option 1</option>
+		<option value = '2'>Option 2</option>
+	</select>
 	<?php
 }
 ?>
