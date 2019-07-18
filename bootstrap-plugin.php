47b4302a7fd98321d7124db2b48c65a061db0859
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-07-18 00:57:02 -0400

    Card Set Up and Info Retrieval Started
    -now adds card to bottom of index.php file when plugin is activated
    -gets the title and text from index.php
       -places them into textboxes so user can see and edit (no action for form yet though)
    -set up a function that can create database tables if multiple cards was needed

--- bootstrap-plugin.php
+++ bootstrap-plugin.php
@@ -8,11 +8,80 @@
    Author URI: http://www.stephanielynnpitman.com
 */
 
+
+
 error_reporting(0);
+register_activation_hook( __FILE__, 'add_card' );
 add_action( 'wp_enqueue_scripts', 'add_bootstrap');
 add_action('admin_menu','wbs_add_menu');
 add_action('admin_init','wbs_settings_init');
 
+//creates the card on the bottom of the index page when the plugin is activated
+function add_card(){
+	$indexFile = get_template_directory()."/index.php";
+	$file = fopen($indexFile, 'a');
+	$new_card = "\n".'<div class="card"><div class="card-title">CARD TITLE</div><div class="card-body">SOME TEXT AND STUFF</div></div>';
+	fwrite($file, $new_card);
+	fclose($file);
+}
+
+//optional function that can be used later if multiple cards are wanted
+/*global $card_db_version;
+$card_db_version = '1.0';
+function card_db_install(){	
+
+	global $wpdb;
+	global $card_db_version;
+
+	$message = $wpdb->prefix;
+	echo "<script type='text/javascript'>alert('$message');</script>";
+
+	$table_name = $wpdb->prefix . 'cards';
+	
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		cardId mediumint(9) NOT NULL AUTO_INCREMENT,
+		title text NOT NULL,
+		PRIMARY KEY  (cardId)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'card_db_version', $card_db_version );
+
+	$table_name = $wpdb->prefix . 'cards_titles';
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		titleId mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		cardId mediumint(9) NOT NULL,
+		title text NOT NULL,
+		FOREIGN KEY (cardId) REFERENCES cards(cardId)
+	) $charset_collate;";	
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'card_db_version', $card_db_version );
+
+	$table_name = $wpdb->prefix . 'cards_text';
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		textId mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		cardId mediumint(9) NOT NULL,
+		text text NOT NULL,
+		FOREIGN KEY (cardId) REFERENCES cards(cardId)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'card_db_version', $card_db_version );
+
+}*/
 
 function add_bootstrap(){
 	//option to use boostrap file inside this plugin directory
@@ -30,34 +99,45 @@ function wbs_page_create(){
 	?>
 	<form method="POST">
 		<h2>Website Card Settings</h2>
+		
 		<?php
-		//add cards button
-		//a section for each card currently already in site with...
-			//a title textbox
-			//a body textbox
-			//a delete button for this card
-		//submit button for changes made
+		//finds the line that produces the card
+		$file = fopen(get_template_directory()."/index.php", 'r');
+		$line = "";
+		$prevLine = "";
+		$line = fgets($file);
+		while($line != ""){
+			$prevLine = $line;
+			$line = fgets($file);
+		}
+
+		//gets the card's title and text
+		$str_arr = preg_split ("/\=/", $prevLine);  
+
+		//cardTitle
+		//trim beginning
+		$cardTitle = substr($str_arr[2], strpos($str_arr[2], '">')+2, strlen($str_arr[2]));
+		//trim ending
+		$cardTitle = substr($cardTitle, 0, strpos($cardTitle,"</"));
+
+		//cardText
+		//trim beginning
+		$cardText = substr($str_arr[3], strpos($str_arr[3], '">')+2, strlen($str_arr[3]));
+		//trim ending
+		$cardText = substr($cardText, 0, strpos($cardText,"</"));
+		fclose($file);
+		
+		//places both title and text into textboxes
 		?>
+		<input type="text" id="cardTitleInput" value="<?php echo $cardTitle; ?>">
+		<input type="text" id="cardTextInput" value="<?php echo $cardText; ?>">
 			
 	</form>
-
-
-
-
-
 	<?php
 	
 	/*settings_fields('wbsHeaderPlugin');
-	do_settings_sections('wbsHeaderPlugin');
-	submit_button();*/
+	do_settings_sections('wbsHeaderPlugin');*/
 
-	//code that adds a card at the bottom of the index page
-	//currently runs everytime the settings page loads (for testing)
-	$indexFile = get_template_directory()."/index.php";
-	$file = fopen($indexFile, 'a');
-	$new_card = "\n".'<div class="card"><div class="card-body">SOME TEXT AND STUFF</div></div>';
-	fwrite($file, $new_card);
-	fclose($file);
 	submit_button("Save Changes");
 	?>
 	
