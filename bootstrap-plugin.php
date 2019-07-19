2ec42e62dcd964d66ca8afe4b8c11cd9a7e7fd3f
StephPitman <32081012+StephPitman@users.noreply.github.com>
2019-07-18 23:41:05 -0400

    Final Submission
    -user input into textboxes will show up as the card's title and text respectively

--- bootstrap-plugin.php
+++ bootstrap-plugin.php
@@ -8,8 +8,6 @@
    Author URI: http://www.stephanielynnpitman.com
 */
 
-
-
 error_reporting(0);
 register_activation_hook( __FILE__, 'add_card' );
 add_action( 'wp_enqueue_scripts', 'add_bootstrap');
@@ -25,64 +23,7 @@ function add_card(){
 	fclose($file);
 }
 
-//optional function that can be used later if multiple cards are wanted
-/*global $card_db_version;
-$card_db_version = '1.0';
-function card_db_install(){	
-
-	global $wpdb;
-	global $card_db_version;
-
-	$message = $wpdb->prefix;
-	echo "<script type='text/javascript'>alert('$message');</script>";
-
-	$table_name = $wpdb->prefix . 'cards';
-	
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		cardId mediumint(9) NOT NULL AUTO_INCREMENT,
-		title text NOT NULL,
-		PRIMARY KEY  (cardId)
-	) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-
-	add_option( 'card_db_version', $card_db_version );
-
-	$table_name = $wpdb->prefix . 'cards_titles';
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		titleId mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		cardId mediumint(9) NOT NULL,
-		title text NOT NULL,
-		FOREIGN KEY (cardId) REFERENCES cards(cardId)
-	) $charset_collate;";	
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-
-	add_option( 'card_db_version', $card_db_version );
-
-	$table_name = $wpdb->prefix . 'cards_text';
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		textId mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		cardId mediumint(9) NOT NULL,
-		text text NOT NULL,
-		FOREIGN KEY (cardId) REFERENCES cards(cardId)
-	) $charset_collate;";
-
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-	dbDelta( $sql );
-
-	add_option( 'card_db_version', $card_db_version );
-
-}*/
-
+//enqueues bootstrap into the style of the website
 function add_bootstrap(){
 	//option to use boostrap file inside this plugin directory
 	//wp_enqueue_style("bootstrap",content_url()."/plugins/bootstrap-steph/css/bootstrap.min.css");
@@ -91,19 +32,38 @@ function add_bootstrap(){
 	wp_enqueue_style("boostrap","https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
 }
 
+//adds the settings menu for the plugin in the "Settings" menu
 function wbs_add_menu(){
-	add_options_page('WordPress and Bootstrap Settings Page','WordPress and Bootstrap Settings Page','manage_options','wbs-api-page','wbs_page_create');
+	add_options_page('Card Settings Page','Card Settings Page','manage_options','wbs-api-page','wbs_page_create');
 }
 
+//creates the settings page
 function wbs_page_create(){
+	if(isset($_POST["cardTitleInput"]) && isset($_POST["cardTextInput"])){
+		//read file into array
+		$fileLines = file(get_template_directory()."/index.php");
+ 
+		//pop old card html line
+		array_pop($fileLines);
+		//append new card html line
+		array_push($fileLines, "\n".'<div class="card"><div class="card-title">'.$_POST["cardTitleInput"].'</div><div class="card-body">'.$_POST["cardTextInput"].'</div></div>');
+ 
+		//connect array elements 
+		$f = join('', $fileLines);
+ 
+		//write updated file back to index.php
+		$fileOpen = fopen(get_template_directory()."/index.php", 'w');
+		fputs($fileOpen, $f);
+		fclose($fileOpen);
+	}
 	?>
-	<form method="POST">
+	<form method="post">
 		<h2>Website Card Settings</h2>
 		
 		<?php
-		//finds the line that produces the card
+		//finds the line that produces the card (stored at the very bottom of index.php)
 		$file = fopen(get_template_directory()."/index.php", 'r');
-		$line = "";
+		//$line = "";
 		$prevLine = "";
 		$line = fgets($file);
 		while($line != ""){
@@ -129,33 +89,20 @@ function wbs_page_create(){
 		
 		//places both title and text into textboxes
 		?>
-		<input type="text" id="cardTitleInput" value="<?php echo $cardTitle; ?>">
-		<input type="text" id="cardTextInput" value="<?php echo $cardText; ?>">
-			
+		Card Title:<input type="text" name="cardTitleInput" value="<?php echo $cardTitle; ?>">
+		Card Text:<input type="text" name="cardTextInput" value="<?php echo $cardText; ?>">
+		<?php
+		submit_button("Save Changes");
+		?>		
+	
 	</form>
 	<?php
-	
-	/*settings_fields('wbsHeaderPlugin');
-	do_settings_sections('wbsHeaderPlugin');*/
-
-	submit_button("Save Changes");
-	?>
-	
-	<?php
 }
 
+//initializes the settings for the plugin
 function wbs_settings_init(){
-	register_setting('wbsHeaderPlugin','wbs_header_settings');
-	add_settings_section('wbs_plugin_header_section',__('Header Settings','wordpress'), 'wbs_section_call', 'wbsHeaderPlugin');
-	add_settings_field('wbs_header_colour',__('Colour','wordpress'), 'wbs_select_field_call', 'wbsHeaderPlugin','wbs_plugin_header_section');
-}
-
-function wbs_section_call(){
-	echo __('Customize Your Website Header','wordpress');
-}
-
-function wbs_select_field_call(){
-	$options = get_option('wbs_header_settings');
-	
+	register_setting('wbsCardPlugin','wbs_card_settings');
+	add_settings_section('wbs_plugin_card_section',__('Card Settings','wordpress'), 'wbs_section_call', 'wbsCardPlugin');
+	add_settings_field('wbs_card',__('Card','wordpress'), 'wbs_select_field_call', 'wbsCardPlugin','wbs_plugin_card_section');
 }
 ?>
